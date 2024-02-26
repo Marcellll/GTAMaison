366ba235799a0edd2b341682d835197f006f6c5b
Marcel Boulot <beyer.abel.marcel@outlook.de>
2024-02-26 16:24:45 +0100

    Changed the display of the information messages. Added the RFID reading for test on the Raspberry

--- GTA.py
+++ GTA.py
@@ -4,10 +4,18 @@ import sys
 from Enums.TypeTravail import *
 from Enums.TypeBagdage import *
 import time
+from RFID_read import *
 
 root = Tk()
 root.title("Badgeuse")
 root.attributes("-fullscreen", True)
+infoText = StringVar()
+infoLabel = Label(root, textvariable=infoText, font=("Verdana", 25)).pack()
+
+def get_RFID_reading():
+    infoText.set("Lecture du badge en attente...")
+    (RFIDCardID, RFIDCardName) = get_reading()
+    return str(RFIDCardID)
 
 def clickExit():
     root.quit()
@@ -16,38 +24,34 @@ def clickSortie():
     semaineActuel = datetime.today().strftime("%W")
     dateActuel = datetime.today().strftime("%d.%m.%Y")
     heureActuel = datetime.today().strftime("%H:%M:%S")
-    RFIDCardActuel = "360976183797"
-    infoPage = Toplevel(root)
-    infoPage.title("Info badgage")
+    RFIDCardActuel = get_RFID_reading()
     con = create_connection()
     if is_RFID_used(con, RFIDCardActuel): 
         if not(get_Personne_TypeTravail(con, RFIDCardActuel) == TypeTravail.JOURNEE): 
             if not(is_Personne_logged_off(con, RFIDCardActuel)):
                 create_new_badge_entry(con, RFIDCardActuel, semaineActuel, dateActuel, heureActuel, TypeBadgage.SORTIE.value)
-                infoLabel = Label(infoPage, text = f"{get_Personne(con, RFIDCardActuel)} a badgé sa sortie le {dateActuel} à {heureActuel}. Bonne journée !").pack()
+                infoText.set(f"{get_Personne(con, RFIDCardActuel)} a badgé sa sortie le {dateActuel} à {heureActuel}. Bonne journée !")
             else:
-                infoLabel = Label(infoPage, text = f"Vous avez déjà badgé votre sortie !").pack()
+                infoText.set(f"Vous avez déjà badgé votre sortie !")
         else:
-            infoLabel = Label(infoPage, text=f"Vous n'avez pas besoin de badger votre sortie").pack()
+            infoText.set(f"Vous n'avez pas besoin de badger votre sortie")
     else:
-        infoLabel = Label(infoPage, text = f"Votre badge n'est pas enregistré, veuillez contacter Solène Dalmar").pack()
+        infoText.set(f"Votre badge n'est pas enregistré, veuillez contacter Solène Dalmar")
 
 def clickEntree():
     semaineActuel = datetime.today().strftime("%W")
     dateActuel = datetime.today().strftime("%d.%m.%Y")
     heureActuel = datetime.today().strftime("%H:%M:%S")
-    RFIDCardActuel = "360976183797"
-    infoPage = Toplevel(root)
-    infoPage.title("Info badgage")
+    RFIDCardActuel = get_RFID_reading()
     con = create_connection()
     if is_RFID_used(con, RFIDCardActuel):  
         if not(is_Personne_logged_in(con, RFIDCardActuel)):
             create_new_badge_entry(con, RFIDCardActuel, semaineActuel, dateActuel, heureActuel, TypeBadgage.ENTREE.value)
-            infoLabel = Label(infoPage, text = f"{get_Personne(con, RFIDCardActuel)} a badgé son entrée le {dateActuel} à {heureActuel}. Merci !").pack()
+            infoText.set(f"{get_Personne(con, RFIDCardActuel)} a badgé son entrée le {dateActuel} à {heureActuel}. Merci !")
         else:
-            infoLabel = Label(infoPage, text = f"Vous avez déjà badgé votre entrée !").pack()
+            infoText.set(f"Vous avez déjà badgé votre entrée !")
     else:
-        infoLabel = Label(infoPage, text = f"Votre badge n'est pas enregistré, veuillez contacter Solène Dalmar").pack()
+        infoText.set(f"Votre badge n'est pas enregistré, veuillez contacter Solène Dalmar")
 
 def adminView():
     adminPage = Toplevel(root)
@@ -61,8 +65,8 @@ def adminView():
     #ajoutNouvelEmployeButton = Button(adminPage, text = "Ajouter une nouvel employé",padx=200, pady = 50, 
                                        #command=lambda: create_new_employee(create_connection(),,, radioButtonVariable.get())).pack()
     
-entreeButton = Button(root, text="Entrée", padx = (root.winfo_screenwidth() * 0.15), pady = (root.winfo_screenheight() * 0.2),bg="green", command=clickEntree)
-sortieButton = Button(root, text="Sortie", padx = (root.winfo_screenwidth() * 0.15), pady = (root.winfo_screenheight() * 0.2), bg="red", command=clickSortie)
+entreeButton = Button(root, text="Entrée", padx = (root.winfo_screenwidth() * 0.15), pady = (root.winfo_screenheight() * 0.2),bg="green", font = ("Verdana", 30), fg = "#FFFFFF", command=clickEntree)
+sortieButton = Button(root, text="Sortie", padx = (root.winfo_screenwidth() * 0.15), pady = (root.winfo_screenheight() * 0.2), bg="red", font = ("Verdana", 30), fg = "#FFFFFF", command=clickSortie)
 exitButton = Button(root, text="Quitter", command=clickExit)
 adminButton = Button(root, text="Admin", command=adminView)
 
